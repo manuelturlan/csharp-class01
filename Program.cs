5561a0cf856ec65f68563328e25a837e5a00d3be
manuelturlan <manuelturlan18@gmail.com>
2022-12-10 22:14:21 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// 01
+Console.Write("Type your name: ");
+string name = Console.ReadLine();
+Console.WriteLine("Hi " + name + "!");
+
+// 02
+Console.Write("The current time is: " + DateTime.Now.TimeOfDay);
